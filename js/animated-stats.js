7dec7a17402f88f26955c1f459ab9d6b91f5b342
sophie-wjyang <sophie.wjyang@gmail.com>
2022-06-29 11:47:22 -0400

    sticky nav bar, animated stats bar

--- js/animated-stats.js
+++ js/animated-stats.js
@@ -1,26 +1,27 @@
-// "counters" is a reference to all elements with the class "value"
+// "counters" is an array of all elements with the class "value"
 let counters = document.querySelectorAll(".value");
-const time = 5000; // 5000 milliseconds
+
+const duration = 5000;
 
 counters.forEach((counter) => {
 	const animate = () => {
         // get end value that we stop at
-		const value = +counter.getAttribute("end-val");
-		console.log("value: " + value);
-
-		// increment_speed is how much we increment the number by every period
-        const increment_speed = value/time;
-		console.log("increment speed: " + increment_speed);
+		const value = +counter.getAttribute("end_val");
 
         // get current rendered value
 		const data = +counter.innerText;
-		console.log("data: " + data);
+        console.log("-endval: " + value + ", data: " + data);
 
-        // if we haven't reached the end value yet, 
+		// how frequently we update the displayed number
+		const period = duration/value;
+        console.log("period: " + period);
+
+
+        // if we haven't reached the end value yet, keep incrementing
 		if (data < value) {
-			counter.innerText = Math.ceil(data + increment_speed);
-			console.log("innertext: " + counter.innerText);
-			setTimeout(animate, time/value);
+			counter.innerText = data + 1;
+			// call animate() recursively
+			setTimeout(animate, period);
 		} 
 
         // stay at end value once we've reached it
