b50161da5ecb8af86aa6f415d6b69196a78c2aa2
sophie-wjyang <sophie.wjyang@gmail.com>
2022-06-29 16:09:53 -0400

    finished navigation search animation

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,11 +1,19 @@
 // hide other navbar items when user clicks into search bar
 function navSearchFocus() {
-    var navItems = document.getElementById("navbar-items");
+    var navItems = document.getElementById("hide-navbar-items");
+    // var brandLogo = document.getElementById("hide-brand-logo");
+    // var navBar = document.getElementById("navbar-two-custom");
+
     navItems.style.display = "none";
+    // brandLogo.style.display = "none";
+    // navBar.style.height = "64.8px";
 }
 
 // show other navbar items when user clicks out of search bar
 function navSearchBlur() {
-    var navItems = document.getElementById("navbar-items");
+    var navItems = document.getElementById("hide-navbar-items");
+    // var brandLogo = document.getElementById("hide-brand-logo");
+
     navItems.style.display = "";
+    // brandLogo.style.display = "";
 }
